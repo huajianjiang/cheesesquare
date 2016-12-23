3b2d8b9b5cba6c3d966802911838d38d54436c36
huajianjiang <jhj.andever@gmail.com>
2016-12-23 22:45:18 +0800

    init

--- app/src/main/java/com/support/android/designlibdemo/MainActivity.java
+++ app/src/main/java/com/support/android/designlibdemo/MainActivity.java
@@ -35,9 +35,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -83,7 +80,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
-        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.setupWithViewPager(viewPager,true);
     }
 
     @Override
